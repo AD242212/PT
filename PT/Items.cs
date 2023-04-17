ae6f08cb38ae969c1a777ad761b090c93c35fb2a
AD242212 <mystogun618@gmail.com>
2023-04-17 18:52:19 +0200

    items

---
+++ PT/Items.cs
@@ -0,0 +1,34 @@
+﻿namespace PT
+{
+    abstract class Item
+    {
+        private int id;
+        private string name;
+        private float price;
+        private int nums_in_stock;
+
+        public Item(int id, string name, float price, int nums_in_stock)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.nums_in_stock = nums_in_stock;
+        }
+
+        public Item()
+        {
+
+        }
+
+    }
+
+    class Phone : Item
+    {
+
+    }
+
+    class Laptop : Item
+    {
+
+    }
+}
